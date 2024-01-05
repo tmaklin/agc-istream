87b50d5158d62758c8bfce847dfe5ffacfe48d2b
Tommi Mäklin <tommi@maklin.fi>
2024-01-05 14:34:39 +0200

    Converg agc_open_istream to the AgcStreamer class.

--- agc_istream.cpp
+++ agc_istream.cpp
@@ -10,120 +10,124 @@
 #include "genome_io.h" // CGenomeIO
 #include "defs.h" // contig_t
 
-std::istringstream open_agc_istream(const std::string &sample_name) {
+class AgcStreamer : public CAGCDecompressorLibrary {
+private:
     size_t line_length = 80;
     size_t no_threads = 1;
 
-    CAGCDecompressorLibrary agc_d(true);
-    agc_d.Open("ref.agc", false);
-
-    sample_desc_t sample_desc;
-
-    if (!agc_d.collection_desc->get_sample_desc(sample_name, sample_desc)) {
-	throw std::runtime_error("There is no sample " + sample_name);
+public:
+    AgcStreamer(std::string _archive_path) : CAGCDecompressorLibrary(true) {
+	this->Open(_archive_path);
     }
 
-    agc_d.q_contig_tasks = std::make_unique<CBoundedQueue<CAGCDecompressorLibrary::contig_task_t>>(1, 1);
-    agc_d.pq_contigs_to_save = std::make_unique<CPriorityQueue<CAGCDecompressorLibrary::sample_contig_data_t>>(no_threads * 1);
+    std::istringstream get(const std::string &sample_name) {
+	sample_desc_t sample_desc;
 
-    std::vector<CAGCDecompressorLibrary::contig_task_t> v_tasks;
-    std::vector<std::thread> v_threads;
+	if (!this->collection_desc->get_sample_desc(sample_name, sample_desc)) {
+	    throw std::runtime_error("There is no sample " + sample_name);
+	}
 
-    // Saving thread
-    std::thread gio_thread([&] {
-	CGenomeIO gio;
-	CAGCDecompressorLibrary::sample_contig_data_t ctg;
+	this->q_contig_tasks = std::make_unique<CBoundedQueue<CAGCDecompressorLibrary::contig_task_t>>(1, 1);
+	this->pq_contigs_to_save = std::make_unique<CPriorityQueue<CAGCDecompressorLibrary::sample_contig_data_t>>(no_threads * 1);
 
-	gio.Open(sample_name, true);
+	std::vector<CAGCDecompressorLibrary::contig_task_t> v_tasks;
+	std::vector<std::thread> v_threads;
 
-	while (!agc_d.pq_contigs_to_save->IsCompleted())
-	    {
-		if (!agc_d.pq_contigs_to_save->Pop(ctg))
-		    break;
+	// Saving thread
+	std::thread gio_thread([&] {
+	    CGenomeIO gio;
+	    CAGCDecompressorLibrary::sample_contig_data_t ctg;
 
-		gio.SaveContig(ctg.contig_name, ctg.contig_data, line_length);
-	    }
+	    gio.Open(sample_name, true);
 
-	gio.Close();
-    });
+	    while (!this->pq_contigs_to_save->IsCompleted())
+		{
+		    if (!this->pq_contigs_to_save->Pop(ctg))
+			break;
 
-    v_threads.clear();
-    v_threads.reserve(no_threads);
-
-    std::ostringstream out;
-
-    v_tasks.clear();
-    v_tasks.shrink_to_fit();
-    v_tasks.reserve(sample_desc.size());
-
-    for (size_t i = 0; i < sample_desc.size(); ++i)
-	v_tasks.emplace_back(i, "", sample_desc[i].first, sample_desc[i].second);
-
-    std::sort(v_tasks.begin(), v_tasks.end(), [](auto& x, auto& y) {return x.segments.size() > y.segments.size(); });
-
-    agc_d.q_contig_tasks->Restart(1);
-
-    size_t i = 0;
-    bool converted_to_alpha = true;
-    v_threads.emplace_back([&agc_d, i, converted_to_alpha, &out] {
-
-	ZSTD_DCtx *zstd_ctx = ZSTD_createDCtx();
-
-	std::vector<uint8_t> ctg;
-	CAGCDecompressorLibrary::contig_task_t contig_desc;
-
-	while (!agc_d.q_contig_tasks->IsCompleted())
-	    {
-		if (!agc_d.q_contig_tasks->Pop(contig_desc))
-		    break;
-
-		agc_d.decompress_contig(contig_desc, zstd_ctx, ctg);
-		agc_d.convert_to_alpha(ctg);
-
-		out << ">" << contig_desc.name_range.str() << '\n';
-		size_t nt_count = 0;
-		for (auto nt : ctg) {
-		    out << nt;
-		    ++nt_count;
-		    if (nt_count%80 == 0) {
-			out << '\n';
-		    }
+		    gio.SaveContig(ctg.contig_name, ctg.contig_data, line_length);
 		}
-		out << std::endl;
-	    }
 
-	agc_d.pq_contigs_to_save->MarkCompleted();
+	    gio.Close();
+	});
 
-	ZSTD_freeDCtx(zstd_ctx);
-    });
+	v_threads.clear();
+	v_threads.reserve(no_threads);
 
-    for (auto& task : v_tasks)
-	agc_d.q_contig_tasks->Push(task, 0);
-    agc_d.q_contig_tasks->MarkCompleted();
+	std::ostringstream out;
 
-    agc_d.join_threads(v_threads);
+	v_tasks.clear();
+	v_tasks.shrink_to_fit();
+	v_tasks.reserve(sample_desc.size());
 
-    v_threads.clear();
+	for (size_t i = 0; i < sample_desc.size(); ++i)
+	    v_tasks.emplace_back(i, "", sample_desc[i].first, sample_desc[i].second);
 
-    gio_thread.join();
+	std::sort(v_tasks.begin(), v_tasks.end(), [](auto& x, auto& y) {return x.segments.size() > y.segments.size(); });
 
-    agc_d.q_contig_tasks.release();
-    agc_d.pq_contigs_to_save.release();
+	this->q_contig_tasks->Restart(1);
 
-    return std::istringstream(out.str());
-}
+	size_t i = 0;
+	bool converted_to_alpha = true;
+	v_threads.emplace_back([this, i, converted_to_alpha, &out] {
+
+	    ZSTD_DCtx *zstd_ctx = ZSTD_createDCtx();
+
+	    std::vector<uint8_t> ctg;
+	    CAGCDecompressorLibrary::contig_task_t contig_desc;
+
+	    while (!this->q_contig_tasks->IsCompleted())
+		{
+		    if (!this->q_contig_tasks->Pop(contig_desc))
+			break;
+
+		    this->decompress_contig(contig_desc, zstd_ctx, ctg);
+		    this->convert_to_alpha(ctg);
+
+		    out << ">" << contig_desc.name_range.str() << '\n';
+		    size_t nt_count = 0;
+		    for (auto nt : ctg) {
+			out << nt;
+			++nt_count;
+			if (nt_count%80 == 0) {
+			    out << '\n';
+			}
+		    }
+		    out << std::endl;
+		}
+
+	    this->pq_contigs_to_save->MarkCompleted();
+
+	    ZSTD_freeDCtx(zstd_ctx);
+	});
+
+	for (auto& task : v_tasks)
+	    this->q_contig_tasks->Push(task, 0);
+	this->q_contig_tasks->MarkCompleted();
+
+	this->join_threads(v_threads);
+
+	v_threads.clear();
+
+	gio_thread.join();
+
+	this->q_contig_tasks.release();
+	this->pq_contigs_to_save.release();
+
+	return std::istringstream(out.str());
+    }
+};
 
 int main() {
     std::string sample_name = "ref";
 
-    std::istringstream in = open_agc_istream(sample_name);
-    
-    std::string line;
+    AgcStreamer streamer("ref.agc");
+    std::istringstream in = streamer.get(sample_name);
     size_t line_nr = 0;
+    std::string line;
     while (std::getline(in, line)) {
 	std::cerr << line_nr << '\t' << line << std::endl;
 	++line_nr;
     }
-
     return 1;
 }
